91334bee3a951c9b19e1612934ed6d15adac103d
Muzammal Hussain <muzammal.hussain@argonteq.com>
2024-03-29 12:59:49 +0500

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,20 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+const CriticScore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 65 ? "yellow" : "";
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"14px"}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
